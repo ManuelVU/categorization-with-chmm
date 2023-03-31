d6002d43adbb071518f784313617c800d9a8d8d5
Manuel <jesusmv1@uci.edu>
2023-03-31 14:36:54 -0700

    adding category stimulus conditions for lewandowskys experiment

--- src/lewandowsky-rw.R
+++ src/lewandowsky-rw.R
@@ -1,8 +1,9 @@
 # Function that reads and writes data from one or more conditions in
 # Lewandowsky (2011), file is whiten in long-format as a .csv into the 
-# ~/data/csv-files directory.
+# ~/data/csv-files directory. (requires the file: 
+# data/csv-files/lewandowsky-categories.csv)
 lewandowsky_rw <- function(file_suffix, 
                            conditions_keep = c(1:4), 
                            participants_keep = c(1:113)){
-  
+  cats <- readr::read_csv(file = "data/csv-files/lewandowsky-categories.csv")
 }
