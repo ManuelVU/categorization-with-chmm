a890c4464b0a73810ef3cb4e46fcd7c20db3e810
Manuel <jesusmv1@uci.edu>
2023-04-05 13:20:39 -0700

    added a description of the organization of the output of the distinctive attributes function

--- src/similarity-f/lee-navarro-distinctive.R
+++ src/similarity-f/lee-navarro-distinctive.R
@@ -2,6 +2,10 @@
 # arrays named for features in stimulus i not shared with stimulus j 
 # (in_i_not_in_j) and features that are not in i but are in j (not_in_i_in_j)
 # distinctive features between all of them.
+
+# Formally, the function returns a matrix with f_ik (1-f_jk) and (1-f_ik) f_jk
+# with dimensions organised in multiple matrix that contain all other stimulus
+# being compared in rows and features in columns
 distinctive_ln <- function(stimulus_features){
   
   n_stimulus <- nrow(stimulus_features)
@@ -16,10 +20,11 @@ distinctive_ln <- function(stimulus_features){
                                                  n_features, 
                                                  n_stimulus)))
   for(i in 1:n_stimulus){
+    
     st_features <- unlist(stimulus_features[i, (3:ncol(stimulus_features))]) |> 
       as.vector() |> 
       rep(times = n_stimulus - 1) |> 
-      matrix(nrow = n_stimulus - 1)
+      matrix(nrow = n_stimulus - 1, byrow = TRUE)
     
     st_index_comp <- stimulus_features[-i, 1]
     
@@ -27,7 +32,8 @@ distinctive_ln <- function(stimulus_features){
       as.matrix()
     
     output$in_i_not_in_j[,,i] <- st_features * (1 - st_features_comp)
-    output$not_in_i_in_j[,,i] <- st_features * (1 - st_features_comp)
+    
+    output$not_in_i_in_j[,,i] <- (1 - st_features) * st_features_comp
     
   }
   return(output)
