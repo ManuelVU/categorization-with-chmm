b8bb5d2f0bd898180d92e46c8cee5123d1dade9a
Manuel <jesusmv1@uci.edu>
2023-06-22 11:42:49 -0700

    minkowsky distance function is now working on two dimensional feature spaces

--- src/similarity-f/minkowsky-distance.R
+++ src/similarity-f/minkowsky-distance.R
@@ -2,15 +2,23 @@
 # n_stimulus x n_stimulus with the Minkowski of order (`order`) distance from 
 # the stimulus indexed by the row number and the stimulus indexed by the column
 minkowski_distance <- function (features, order) {
-  n_stimulus <- nrow(features)
-  feature_values <- features[,-c(1,2)]
+  
+  feature_values <- cbind(features$feature_1, features$feature_2)
+  
+  n_stimulus <- nrow(feature_values)
   
   d_ij <- matrix(data = NA, nrow = n_stimulus, ncol = n_stimulus)
   
   for (i in 1:n_stimulus) {
     for (j in 1:n_stimulus) {
-      d_ij[i, j] <- (feature_values[i, ] %*% t(-feature_values[j, ])) ^ (1 / order)
+      
+      d_ij[i, j] <- sum(abs(feature_values[i, ] - feature_values[j, ]) ^ order)
+      
+      d_ij[i, j] <- d_ij[i, j] ^ (1 / order)
+    
     }
   }
+  
   return(d_ij)
+  
 }
