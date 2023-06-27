775bfca017f24a3b4c2bd3adfeeaaadf1c5f32fc
Manuel <jesusmv1@uci.edu>
2023-06-26 17:17:40 -0700

    making changes to trialn by trial ploting function to allow for different stimulus organization

--- src/plot-f/trial-trial-plot.R
+++ src/plot-f/trial-trial-plot.R
@@ -1,8 +1,8 @@
 # Plotting function for trial by trial responses single participant
 
 trial_trial_participant <- function (data, posteriors, participant_id,
-                                     posterior_add = FALSE, 
-                                     stimulus_plot,
+                                     posterior_add = FALSE, transfer = FALSE,
+                                     stimulus_plot, plot_order,
                                      category_color, border_color,
                                      width = 0.8, 
                                      height = 0.8, 
@@ -13,38 +13,43 @@ trial_trial_participant <- function (data, posteriors, participant_id,
   
   n_trials <- data$participant_t[participant_id]
   
+  if (missing(plot_order)){
+    plot_order <- 1:dim(data$response)[1]
+  }
+  
   if (missing(stimulus_plot)) {
     responses <- data$response[, 1:n_trials, participant_id]
-    
+    responses <- responses[plot_order, ]
   }
   else {
-    responses <- data$responses[stimulus_plot, 1:n_trials, participant_id]
+    responses <- data$response[stimulus_plot, 1:n_trials, participant_id]
   }
   
   n_stimulus <- dim(responses)[1]
   
   if (posterior_add == TRUE) {
     if (!missing(posteriors)) {
-      states_tmp <- 
-        posteriors$posterior_samples$hidden_states[, ,participant_id, ]
-      
-      states_tmp <- states_tmp[, 1:n_trials, ]
+      if (missing(stimulus_plot)) {
+        states_tmp <- 
+          posteriors$posterior_samples$hidden_states[, ,participant_id, ]
+        
+        states_tmp <- states_tmp[, 1:n_trials, ]
+        
+        states_tmp <- states_tmp[plot_order, , ]
+      }
+      else {
+        states_tmp <- 
+          posteriors$posterior_samples$hidden_states[, ,participant_id, ]
+        
+        states_tmp <- states_tmp[stimulus_plot, 1:n_trials, ]
+        
+        states_tmp <- states_tmp[plot_order, , ]
+      }
     }
   }
   
   plot(x = 0, y = 0, type = "n", ann = FALSE, axes = FALSE, 
        xlim = c(0, n_trials + 1), ylim = c(0,n_stimulus + height))
-  # if (shade_stimulus == TRUE) {
-  #   for (i in 2:n_stimulus) {
-  #     if(i %% 2 == 0){
-  #       rect(xleft = - (width / 2 + region[2]), 
-  #            ybottom = i - 1 + height / 2 + region[1],
-  #            xright = n_trials + width / 2 + region[4],
-  #            ytop = i + height / 2 + region[3], border = FALSE, 
-  #            col = color_shade)
-  #     }
-  #   }
-  # }
   if (shade_stimulus == TRUE) {
     for (i in 1:n_stimulus) {
         rect(xleft = - (width / 2 + region[2]), 
@@ -55,14 +60,16 @@ trial_trial_participant <- function (data, posteriors, participant_id,
     }
   }
   
-  for(tt in 1:n_trials) {
-    rect(xleft = tt - width / 2, 
-         ybottom = which(!is.na(responses[, tt])) - height / 2,
-         xright = tt + width / 2, 
-         ytop = which(!is.na(responses[, tt])) + height / 2,
-         border = border_color,
-         col = 
-           category_color[responses[which(!is.na(responses[, tt])), tt] + 1])
+  if (transfer == FALSE) {
+    for(tt in 1:n_trials) {
+      rect(xleft = tt - width / 2, 
+           ybottom = which(!is.na(responses[, tt])) - height / 2,
+           xright = tt + width / 2, 
+           ytop = which(!is.na(responses[, tt])) + height / 2,
+           border = border_color,
+           col = 
+             category_color[responses[which(!is.na(responses[, tt])), tt] + 1])
+    }  
   }
   
   if (posterior_add == TRUE) {
@@ -87,3 +94,4 @@ trial_trial_participant <- function (data, posteriors, participant_id,
     abline(h = mid, col = border_color, lwd = 1.3)
   }
 }
+
