1f75a99ba7a7d9d430451f96e7cbaa720499d4eb
Manuel <jesusmv1@uci.edu>
2023-11-02 16:26:23 -0700

    remove analysis and results from the bartlame experiment

--- analysis/data-analysis/filter-data-by-criterion.R
+++ analysis/data-analysis/filter-data-by-criterion.R
@@ -1,7 +1,5 @@
 # Evaluate participant's behavior and create a data files with only those who 
 # reach criterion
-
-source("src/data-f/bartlema-rw.R")
 source("src/data-f/lee-navarro-rw.R")
 
 source("analysis/performance-evaluation/learning-rate.R")
@@ -12,11 +10,3 @@ lee_navarro_rw(file_suffix = "type1-filtered", conditions_keep = 1,
 
 lee_navarro_rw(file_suffix = "type4-filtered", conditions_keep = 4, 
                participants_keep = participants_to_keep_lee)
-
-bartlema_rw(design = "Diagonal", add_transfer = TRUE, 
-            participants_keep = participants_to_keep_diagonal, 
-            file_suffix = "filtered")
-
-bartlema_rw(design = "CrissCross", add_transfer = TRUE, 
-            participants_keep = participants_to_keep_criss, 
-            file_suffix = "filtered")
